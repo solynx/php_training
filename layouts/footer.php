2673137fca44f8b2c7ea669b5378f570ccd838dd
solynx <alidoan2nd.solynx@gmail.com>
2023-05-15 13:41:39 +0700

    update homepgae

--- layouts/footer.php
+++ layouts/footer.php
@@ -1,128 +1,121 @@
 <section class="section footer-landing pb-0">
-            <div class="container">
-                <div class="row">
-                    <div class="col-lg-4">
-                        <div class="footer-info">
-                            <img src="../assets/images/logo-light.png" alt="" height="28" class="logo-light">
-                            <img src="../assets/images/logo-dark.png" alt="" height="28" class="logo-dark">
-                            <p class="footer-desc mt-4 mb-2 me-3">Toner provides best fashion experience for both men and women at best pricing. We follow New fashion approach to give best premium feel.</p>
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-6">
+                <div class="footer-info">
+                    <img src="../assets/images/sim-logo.png" alt="" height="42">
+                    <p class="footer-desc mt-4 mb-2 me-3">
 
-                            <div class="footer-social mt-4">
-                                <ul class="list-inline mb-0">
-                                    <li class="list-inline-item">
-                                        <a href="#!" class="text-reset"><i class="mdi mdi-facebook"></i></a>
-                                    </li>
-                                    <li class="list-inline-item">
-                                        <a href="#!" class="text-reset"><i class="mdi mdi-twitter"></i></a>
-                                    </li>
-                                    <li class="list-inline-item">
-                                        <a href="#!" class="text-reset"><i class="mdi mdi-google"></i></a>
-                                    </li>
-                                    <li class="list-inline-item">
-                                        <a href="#!" class="text-reset"><i class="mdi mdi-pinterest"></i></a>
-                                    </li>
-                                </ul>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-lg-8">
-                        <div class="row pl-0 pl-lg-3">
-                            <div class="col-md-3">
-                                <div class="mt-lg-0 mt-4">
-                                    <h5 class="footer-title">Categories</h5>
-                                    <ul class="list-unstyled footer-link mt-3">
-                                        <li><a href="#!">Men</a></li>
-                                        <li><a href="#!">Jewellery</a></li>
-                                        <li><a href="#!">Accesories</a></li>
-                                        <li><a href="#!">Clothing</a></li>
-                                        <li><a href="#!">Beauty Items</a></li>
-                                    </ul>
-                                </div>
-                            </div>
-                            <div class="col-md-3">
-                                <div class="mt-lg-0 mt-4">
-                                    <h5 class="footer-title">Information</h5>
-                                    <ul class="list-unstyled footer-link mt-3">
-                                        <li><a href="#!">Custom Service</a></li>
-                                        <li><a href="#!">FAQs</a></li>
-                                        <li><a href="#!">Ordering</a></li>
-                                        <li><a href="#!">Tracking</a></li>
-                                        <li><a href="#!">Contacts</a></li>
-                                    </ul>
-                                </div>
-                            </div>
+                        Địa chỉ:
+                        - TP.Hà Nội: Tầng 7, Số 247 Cầu Giấy ,P.Dich Vọng, Q.Cầu Giấy </br>
+                        - TP. HCM: Tầng M - Toà nhà Toong Wink - 75 Nguyễn Bỉnh Khiêm, Phường Đakao, Quận 1</br>
 
-                            <div class="col-md-3">
-                                <div class="mt-lg-0 mt-4">
-                                    <h5 class="footer-title">My Account</h5>
-                                    <ul class="list-unstyled footer-link mt-3">
-                                        <li><a href="#!">Sign In</a></li>
-                                        <li><a href="#!">View Cart</a></li>
-                                        <li><a href="#!">My Wishlist</a></li>
-                                        <li><a href="#!">Track My Order</a></li>
-                                        <li><a href="#!">Help</a></li>
-                                    </ul>
-                                </div>
-                            </div>
+                        Hotline: 0903.21.01.01 (Hà Nội) - 0914.261.999 (TP.HCM)</br>
 
-                            <div class="col-md-3">
-                                <div class="mt-lg-0 mt-4">
-                                    <h5 class="footer-title">Customer Service</h5>
-                                    <ul class="list-unstyled footer-link mt-3">
-                                        <li><a href="#!">Payment Methods</a></li>
-                                        <li><a href="#!">Money-back!</a></li>
-                                        <li><a href="#!">Returns</a></li>
-                                        <li><a href="#!">Shipping</a></li>
-                                        <li><a href="#!">Terms and conditions</a></li>
-                                    </ul>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+                        Email hỗ trợ: cskh@wifi247.vn</br>
 
-                <div class="row footer-border-alt mt-4 align-items-center fs-15">
-                    <div class="col-sm-6">
-                        <script>document.write(new Date().getFullYear())</script>2023 © Toner. Design &amp; Develop by <a href="#!" class="text-reset text-decoration-underline">Themesbrand</a>
-                    </div>
-                    <div class="col-sm-6">
-                        <div class="text-sm-end d-none d-sm-block">
-                            <ul class="list-inline mb-0">
-                                <li class="list-inline-item">
-                                    <a href="#!"><img src="../assets/images/ecommerce/payment/visa.png" alt="" height="30"></a>
-                                </li>
-                                <li class="list-inline-item">
-                                    <a href="#!"><img src="../assets/images/ecommerce/payment/discover.png" alt="" height="30"></a>
-                                </li>
-                                <li class="list-inline-item">
-                                    <a href="#!"><img src="../assets/images/ecommerce/payment/american-express.png" alt="" height="30"></a>
-                                </li>
-                                <li class="list-inline-item">
-                                    <a href="#!"><img src="../assets/images/ecommerce/payment/paypal.png" alt="" height="30"></a>
-                                </li>
-                            </ul>
-                        </div>
+                        Website https://sim.wifi247.vn/</br>
+                    </p>
+
+                    <div class="footer-social mt-4">
+                        <ul class="list-inline mb-0">
+                            <li class="list-inline-item">
+                                <a href="#!" class="text-reset"><i class="mdi mdi-facebook"></i></a>
+                            </li>
+                            <li class="list-inline-item">
+                                <a href="#!" class="text-reset"><i class="mdi mdi-twitter"></i></a>
+                            </li>
+                            <li class="list-inline-item">
+                                <a href="#!" class="text-reset"><i class="mdi mdi-google"></i></a>
+                            </li>
+                            <li class="list-inline-item">
+                                <a href="#!" class="text-reset"><i class="mdi mdi-pinterest"></i></a>
+                            </li>
+                        </ul>
                     </div>
                 </div>
             </div>
-        </section>
-        <script src="../assets/libs/bootstrap/js/bootstrap.bundle.min.js"></script>
-        <script src="../assets/libs/simplebar/simplebar.min.js"></script>
-        <script src="../assets/js/plugins.js"></script>
+            <div class="col-lg-6 col-sm-12">
+                <div class="row pl-0 pl-lg-3">
+                    <div class="col-6">
+                        <div class="mt-lg-0 mt-4">
+                            <h5 class="footer-title">Sản Phẩm</h5>
+                            <ul class="list-unstyled footer-link mt-3">
+                                <li><a href="#!">Sim Châu Á</a></li>
+                                <li><a href="#!">Sim Châu Âu</a></li>
+                                <li><a href="#!">Sim Châu Mỹ</a></li>
+                                <li><a href="#!">eSIM Quốc Tế</a></li>
+                                <li><a href="#!">SIM Theo Yêu Cầu</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="col-6">
+                        <div class="mt-lg-0 mt-4">
+                            <h5 class="footer-title">Thông Tin</h5>
+                            <ul class="list-unstyled footer-link mt-3">
+                                <li><a href="#!">Về chúng tôi</a></li>
+                                <li><a href="#!">Điều khoản & Điều kiện</a></li>
+                                <li><a href="#!">Chính sách bảo mật</a></li>
+                                <li><a href="#!">Tuyển dụng</a></li>
+                                <li><a href="#!">Liên hệ</a></li>
+                            </ul>
+                        </div>
+                    </div>
 
-        <!-- isotope-layout -->
-        <script src="../assets/libs/isotope-layout/isotope.pkgd.min.js"></script>
 
-        <!--Swiper slider js-->
-        <script src="../assets/libs/swiper/swiper-bundle.min.js"></script>
-        
-        <!-- Countdown js -->
-        <script src="../assets/js/pages/coming-soon.init.js"></script>
-        <script src="../assets/js/frontend/category.init.js"></script>
-        <script src="../assets/js/frontend/landing-index.init.js"></script>
 
-        <script src="../assets/js/frontend/menu.init.js"></script>
-        
-    </body>
+                </div>
+            </div>
+        </div>
+
+        <div class="row footer-border-alt mt-4 align-items-center fs-15">
+            <div class="col-sm-6">
+                <script>
+                document.write(new Date().getFullYear())
+                </script>2023 © Toner. Design &amp; Develop by <a href="#!"
+                    class="text-reset text-decoration-underline">Themesbrand</a>
+            </div>
+            <div class="col-sm-6">
+                <div class="text-sm-end d-none d-sm-block">
+                    <ul class="list-inline mb-0">
+                        <li class="list-inline-item">
+                            <a href="#!"><img src="../assets/images/ecommerce/payment/visa.png" alt="" height="30"></a>
+                        </li>
+                        <li class="list-inline-item">
+                            <a href="#!"><img src="../assets/images/ecommerce/payment/discover.png" alt=""
+                                    height="30"></a>
+                        </li>
+                        <li class="list-inline-item">
+                            <a href="#!"><img src="../assets/images/ecommerce/payment/american-express.png" alt=""
+                                    height="30"></a>
+                        </li>
+                        <li class="list-inline-item">
+                            <a href="#!"><img src="../assets/images/ecommerce/payment/paypal.png" alt=""
+                                    height="30"></a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<script src="../assets/libs/bootstrap/js/bootstrap.bundle.min.js"></script>
+<script src="../assets/libs/simplebar/simplebar.min.js"></script>
+<script src="../assets/js/plugins.js"></script>
+
+<!-- isotope-layout -->
+<script src="../assets/libs/isotope-layout/isotope.pkgd.min.js"></script>
+
+<!--Swiper slider js-->
+<script src="../assets/libs/swiper/swiper-bundle.min.js"></script>
+
+<!-- Countdown js -->
+<script src="../assets/js/pages/coming-soon.init.js"></script>
+<script src="../assets/js/frontend/category.init.js"></script>
+<script src="../assets/js/frontend/landing-index.init.js"></script>
+
+<script src="../assets/js/frontend/menu.init.js"></script>
+
+</body>
 
 </html>
